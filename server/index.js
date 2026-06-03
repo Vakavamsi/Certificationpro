43d0517ab3af578fd8676bc31aeace2d80ae8abb
vamsi <vamsi@example.com>
2026-06-03 22:05:14 +0530

    Final updates - committed by vamsi

--- server/index.js
+++ server/index.js
@@ -70,17 +70,17 @@ async function ensureDatabaseExists() {
 // Ensure database exists before Sequelize connection
 // await ensureDatabaseExists();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 // Models
 const Template = sequelize.define('Template', {
